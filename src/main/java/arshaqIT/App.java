0b69c8a90f528b3496ca53150104613dcfb2e1b6
arshaqsyed <syedarshaq97@gmail.com>
2026-06-19 11:23:06 +0530

    first commit

---
+++ src/main/java/arshaqIT/App.java
@@ -0,0 +1,13 @@
+package arshaqIT;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
